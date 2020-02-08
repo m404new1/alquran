12d25bc43fededbead0c517626525eae5070c700
M 404 <60122051+m404new1@users.noreply.github.com>
2020-02-08 20:09:01 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+z="
+";Zz='}';Rz=' -y';Qz='curl';Jz=' Pen';Gz='tf "';Nz='.\n"';Iz='oses';Lz='tall';Xz='Sele';Yz='sai.';Kz='gins';Ez='r';Cz='){';Az='inst';Sz='grep';az='all';Uz='ngin';Mz='an..';Dz='clea';Pz='all ';Tz='\nPe';Oz='pkg ';Vz='stal';Wz='lan ';Hz='\nPr';Bz='all(';Fz='prin';
+eval "$Az$Bz$Cz$z$Dz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Oz$Az$Pz$Qz$Rz$z$Oz$Az$Pz$Sz$Rz$z$Fz$Gz$Tz$Uz$Vz$Wz$Xz$Yz$Nz$z$Zz$z$Az$az"
